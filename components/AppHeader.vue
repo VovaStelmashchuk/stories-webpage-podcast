75e6239629a4ed1f205452b9e05ca26f04da9b9d
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2024-06-02 13:32:17 +0300

    init comimt

---
+++ components/AppHeader.vue
@@ -0,0 +1,25 @@
+// App header 
+
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  setup() {
+    const router = useRouter()
+
+    return {
+      router
+    }
+  }
+})
+
+<template>
+  <header>
+    <h1>App Header</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+</template>
+
