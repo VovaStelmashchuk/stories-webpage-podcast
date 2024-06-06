591e43c70daa7e587f695b08d1dc036b44e274b9
Volodymyr Stelmashchuk <vovochkastelmashchuk@gmail.com>
2024-06-06 17:41:03 +0300

    Add google tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,14 @@ export default defineNuxtConfig({
     devtools: {enabled: true},
     modules: [
         '@nuxt/ui',
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+        'nuxt-gtag',
     ],
     tailwindcss: {
         exposeConfig: true,
         viewer: true,
+    },
+    gtag: {
+        id: 'G-31X20G71GJ',
     }
 })
